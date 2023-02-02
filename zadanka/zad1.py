66a66dff790a3f3ec74b7c7d0db5eb3406c77499
Matiks123456 <124251475+Matiks123456@users.noreply.github.com>
2023-02-02 19:49:30 +0100

    Update zad1.py

--- zadanka/zad1.py
+++ zadanka/zad1.py
@@ -6,8 +6,36 @@ Zadanie 1
 3. Zademonstruj instrukcje warunkowa z uzyciem zmiennych z pkt1
 """
 
-# 1
+# 1!!!
 
-# 2
+liczba = 1
+liczba_zmiennoprzecinkowa = 1.0
+srting = maslo
+bool = True
+(po sprawdzeniu, bo zapomniałem)
+nic = None
+# 2!!!
 
-# 3
+print(liczba + liczba_zmiennoprzecinkowa)
+# 2.0
+print(string + " " + string)
+# maslo maslo
+print(liczba + nic)
+ # 1 albo error?
+
+# 3!!!
+bool = True
+liczba = 1
+if(bool):
+  print("Posiadamy zupę grzybową")
+  if liczba >= 20 and <= 28:
+    print("Zupa grzybowa w idealnej temperaturze :)")
+  elif liczba == 0:
+    print("Zupa grzybowa zamarzła!")
+  elif liczba < 0:
+    print("¯\_(ツ)_/¯")
+  elif liczba > 0 and < 20:
+    print("Zupa grzybowa jest zbyt zimna!!!")
+  else:
+    print("Zupa grzybowa jest zbyt gorąca!")
+  
